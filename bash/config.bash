816a31dfa762dff082f28cc7a89b1184f27aac92
Michael J. Schultz <mjschultz@gmail.com>
2012-06-28 17:47:57 -0500

    Add git completion and some other fixes

---
+++ bash/config.bash
@@ -0,0 +1,5 @@
+export LANG="en_US.UTF-8"
+export EDITOR="vim"
+export VISUAL="vim"
+export FIGNORE=".o:.a"
+export HISTIGNORE="&:[bf]g:exit"
--- bash/paths.bash
+++ bash/paths.bash
@@ -1,6 +1,10 @@
 # Set some sensible PATHs
-export PATH=$HOME/bin:$PATH
-export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
+prepend_path "/usr/local/bin"
+prepend_path "$HOME/bin"
+append_path "/usr/local/sbin"
+append_path "/usr/sbin"
+append_path "/sbin"
+
 export MANPATH=$HOME/man:$MANPATH
 export CDPATH=.:$HOME/Projects
 export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
