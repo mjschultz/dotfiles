2ba928168df91d7e73bd9c17cc5d5c670197c6dd
Michael J. Schultz <mjschultz@gmail.com>
2013-10-05 10:36:17 -0500

    Forgot the -p

--- bash/commands.bash
+++ bash/commands.bash
@@ -1,7 +1,7 @@
 function temp {
     # create a temporary directory
     tempdir=/tmp/$(uuidgen)
-    mkdir $tempdir/temp
+    mkdir -p $tempdir/temp
     # change to tempdir and create a subshell
     ( cd $tempdir/temp && /bin/bash )
     # remove tempdir on shell exit
